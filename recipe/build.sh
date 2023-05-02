e5d5875bfadf966813a07b5b68e3228213695525
Bradley Dice <bdice@bradleydice.com>
2023-05-02 16:57:26 -0500

    Committing temporary progress.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,46 +2,48 @@
 
 echo "STARTING BUILD.SH"
 
-mv -v "${SRC_DIR}/targets" "${PREFIX}/targets"
-
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
-# Install to conda style directories
+# Install impl components (include, lib, LICENSE)
 for platform in "x86_64-linux" "ppc64le-linux" "sbsa-linux"; do
-    pushd "${PREFIX}/targets/${platform}"
-    [[ -d lib64 ]] && mv -v lib64 lib
-    popd
+    mkdir -pv "${PREFIX}/targets/${platform}"
+    for i in "include" "lib" "LICENSE"; do
+        mv -v "${SRC_DIR}/targets/${platform}/${i}" "${PREFIX}/targets/${platform}/${i}"
+    done
 done
 
-# Set up symlinks for target platform only
-pushd "${PREFIX}/${targetsDir}"
-for i in `ls`; do
-    if [[ $i == "bin" ]] || [[ $i == "lib" ]] || [[ $i == "include" ]] || [[ $i == "nvvm" ]]; then
-        if [[ $i == "bin" ]]; then
-            mkdir -pv "${PREFIX}/bin"
-            # Use a custom nvcc.profile to handle the fact that nvcc is a symlink.
-            cp -v "${RECIPE_DIR}/nvcc.profile.for_prefix_bin" "${PREFIX}/bin/nvcc.profile"
-            ln -sv "${PREFIX}/${targetsDir}/bin/nvcc" "${PREFIX}/bin/nvcc"
-            ln -sv "${PREFIX}/${targetsDir}/bin/crt" "${PREFIX}/bin/crt"
-        elif [[ $i == "lib" ]]; then
-            mkdir -pv "${PREFIX}/lib"
-            for j in "$i"/*.a*; do
-                # Static libraries are symlinked in $PREFIX/lib
-                ln -sv "${PREFIX}/${targetsDir}/${j}" "${PREFIX}/${j}"
-            done
-            ln -sv "${PREFIX}/${targetsDir}/${i}" "${PREFIX}/${targetsDir}/lib64"
-        elif [[ $i == "nvvm" ]]; then
-            mkdir -pv "${PREFIX}/nvvm"
-            for j in "$i"/*; do
-                ln -sv "${PREFIX}/${targetsDir}/${i}/${j}" "${PREFIX}/${j}"
-            done
-        fi
-    fi
+# Install bin/nvvm components into $PREFIX/cuda for target platform only
+mkdir -pv "${PREFIX}/cuda"
+for i in "bin" "nvvm"; do
+    mv -v "${SRC_DIR}/${targetsDir}/${i}" "${PREFIX}/cuda/${i}"
 done
-popd
 
+# Symlink nvcc and crt headers from $PREFIX/cuda to $PREFIX/bin
+mkdir -pv "${PREFIX}/bin"
+ln -sv "${PREFIX}/cuda/bin/nvcc" "${PREFIX}/bin/nvcc"
+ln -sv "${PREFIX}/cuda/bin/crt" "${PREFIX}/bin/crt"
+
+# Use a custom nvcc.profile to handle the fact that nvcc is a symlink.
+cp -v "${RECIPE_DIR}/nvcc.profile.for_prefix_bin" "${PREFIX}/bin/nvcc.profile"
+
+# TODO: incomplete from here down
+
+# Symlink static libraries into $PREFIX/lib
+#mkdir -pv "${PREFIX}/lib"
+#pushd "${PREFIX}/${targetsDir}"
+#for i in "lib/*.a*"; do
+#    ln -sv "${PREFIX}/${targetsDir}/${i}" "${PREFIX}/${i}"
+#done
+#ln -sv "${PREFIX}/${targetsDir}/${i}" "${PREFIX}/${targetsDir}/lib64"
+#
+## nvvm
+#mkdir -pv "${PREFIX}/nvvm"
+#for j in "$i"/*; do
+#    ln -sv "${PREFIX}/${targetsDir}/${i}/${j}" "${PREFIX}/${j}"
+#done
+#
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
 # Name this script starting with `~` so it is run after all other compiler activation scripts.
